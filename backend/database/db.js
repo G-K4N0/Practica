07412bd91ffcdf36a4ad08fc956dea15999aa2b5
K4T0 <k4t0@localhost.localdomain>
2023-01-25 15:21:47 -0600

    Conexión a bd

---
+++ backend/database/db.js
@@ -0,0 +1,13 @@
+import  Sequelize  from "sequelize";
+
+const db = new Sequelize(
+    process.env.DATABASE, 
+    process.env.USER, 
+    process.env.PASSWORD,
+        {
+            host:process.env.HOST,
+            dialect: process.env.DIALECT 
+        }
+);
+
+export default db;
